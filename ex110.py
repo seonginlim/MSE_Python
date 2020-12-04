1682395484c4a59e898c15e87bbd2e3de241a115
seongin lim <lhndgdg123@naver.com>
2020-12-05 01:08:37 +0900

    sol1

---
+++ ex110.py
@@ -0,0 +1,9 @@
+if True :             #참이니까 아래로 이동
+    if False:         #거짓이므로 else에 해당하는 값이 출력
+        print("1")
+        print("2")
+    else:    
+        print("3")    #따라서 3이 먼저 출력
+else :
+    print("4")        #첫번째 if문이 참이므로 else부분은 생략
+print("5")            #마지막으로 if문과 상관없이 5가 출력
