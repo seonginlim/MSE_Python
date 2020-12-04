06c1fcc82a7da03e4575509fb24199ff0360c2ba
seongin lim <lhndgdg123@naver.com>
2020-12-05 00:36:35 +0900

    sol1

---
+++ ex050.py
@@ -0,0 +1,3 @@
+data = "039490  "         #data변수에 오른쪽 공백이 들어간 숫자를 대입
+data = data.rstrip()      #오른쪽 공백만을 제거하는 rstrip함수를 이용해서 제거후, 그 값을 새로운 data변수에 할당
+print(data)               #print함수를 이용해서 data값을 출력
