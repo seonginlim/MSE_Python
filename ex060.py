91ddea6f9cb38032a776452b2d62b5e6782ebd00
seongin lim <lhndgdg123@naver.com>
2020-12-05 00:40:12 +0900

    sol1

---
+++ ex060.py
@@ -0,0 +1,4 @@
+num = [1, 2, 3, 4, 5]                #num이란 변수에 각 원소를 리스트 형태로 대입
+average = sum(nums) / len(nums)      #평균을 구하는 함수가 없으므로, sum함수를 이용해서 리스트 원소의 총합을 구하고, len함수를 이용해서 리스트 원소의 개수를 구한다
+                                     #두개의 값을 나눠 준 값을 average라는 변수에 할당
+print(average)                       #print함수를 이용해서 average값을 출력
