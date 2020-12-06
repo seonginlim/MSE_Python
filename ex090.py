222adc740224a595d97f27ff1b41c61359877032
seongin lim <lhndgdg123@naver.com>
2020-12-06 10:54:51 +0900

    sol2

--- ex090.py
+++ ex090.py
@@ -1,4 +1,3 @@
 #딕셔너리도 indexing을 할 수 있다.
-#단, value가 아닌 key값으로 indexing을 할 수 있는데
-#존재하지 않는 key값으로 indexing을 하면 에러가 발생한다.
+#단, key값으로 indexing을 할 수 있는데, 존재하지 않는 key값으로 indexing을 하면 에러가 발생한다.
 #따라서 icecream이란 변수 안에 누가바 라는 key값은 없으므로 에러가 발생했다.
