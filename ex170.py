2f9d56734dc23d446e2c4b313906381952e065db
seongin lim <lhndgdg123@naver.com>
2020-12-06 13:24:55 +0900

    sol2

--- ex170.py
+++ ex170.py
@@ -1,4 +1,4 @@
-result = 1               #result라는 변수에 초기값 1을 할당
-for i in range(1, 11):   #range함수를 이용해서 1~10까지의 수만큼 for문을 반복
+result = 1               #result라는 변수에 초기값 1을 저장.
+for i in range(1, 11):   #range함수를 이용해서 1~10까지의 수만큼 for문을 반복.
     result *= i          #result *= i ==> result = result * i 이다. 
-print(result)            #for문을 10까지 다 돌고 print함수를 이용해서 최종 result값을 출력
+print(result)            #for문을 10까지 다 돌고 print함수를 이용해서 최종 result값을 출력.
